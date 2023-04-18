a942b24bf9666d890c280228c5954a012d91dacf
Chisom Nwike <cnwike@parkmyfleet.com>
2023-04-18 23:22:57 +0100

    footer styling update

--- src/features/pagefooter/pagefooter.js
+++ src/features/pagefooter/pagefooter.js
@@ -1,15 +1,25 @@
 import React from "react"
 import PageFooterStyle from "./pagefooterstyle";
+import playstoreimage from "../../images/playstore.png";
+import applestoreimage from "../../images/applestore.png";
 
-function PageFooter(){
+function PageFooter() {
     const classes = PageFooterStyle();
     return (
         <div className={classes.footer}>
-      <div>Footer content</div>
-      <div className={classes.copyright}>
-        &copy; {new Date().getFullYear()} Your Company Name
-      </div>
-    </div>
+            <div className={classes.footerContent}>
+                <div className={classes.footerText}>Footer content</div>
+
+                <div className={classes.storeOption}>
+                    <div className={classes.footerTextTitle}>Join the community</div>
+                    <img className={classes.img} src={playstoreimage} alt="playstore" href="https://google.com"></img>
+                    <img className={classes.img} src={applestoreimage} alt="applestore" href="https://apple.com"></img>
+                </div>
+            </div>
+            <div className={classes.copyright}>
+                Chisom Nwike / &copy; {new Date().getFullYear()} braindieu / <a className={classes.ownerInfo} href="https://www.braindieu.com">www.braindieu.com</a>
+            </div>
+        </div>
     )
 }
 
--- src/features/pagefooter/pagefooterstyle.js
+++ src/features/pagefooter/pagefooterstyle.js
@@ -7,17 +7,50 @@ const PageFooterStyle = makeStyles((theme) => {
             color: 'White'
         },
         footer: {
+            
+          },
+          footerContent:{
             borderTop: '1px solid #ccc',
             borderBottom: '1px solid #ccc',
             display: 'flex',
             justifyContent: 'space-between',
             alignItems: 'center',
             padding: '10px',
+            marginTop: '1.5rem',
+            marginBottom: '1rem',
           },
           copyright: {
+            display: 'flex',
+            justifyContent: 'right',
+            alignItems: 'right',
             fontSize: '0.8rem',
-            color: 'gray',
+            color: '#fff',
+            fontFamily:'GabtonMalgora'
           },
+          ownerInfo:{
+            fontFamily: 'GabtonMalgora',
+            textDecoration:'none',
+            color:'#fff'
+          },
+          img:{
+            width:'20%',
+            height:'auto'
+          },
+          footerText:{
+            fontFamily: 'GabtonMalgora',
+            textDecoration:'none',
+            color:'#fff'
+          },
+          footerTextTitle:{
+            fontFamily: 'GabtonMalgora',
+            textDecoration:'none',
+            color:'#fff',
+            fontSize: '1rem',
+            marginBottom: "0.5rem",
+          },
+          storeOption:{
+            textAlign:'center'
+          }
     };
 })
 
